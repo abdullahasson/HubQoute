4bd91783c25efadcf9b7522584e880a1f1d8b67d
abdullahasson <abdullahasson@users.noreply.github.com>
2024-10-26 12:13:39 +0000

    deploy: 93c26553a269b5aa1538eb1d494ed4f7b0fab735

---
+++ assets/supplier_card-DEtLAOu5.js
@@ -0,0 +1,1 @@
+import{j as r,B as a,D as s}from"./index-D_UmtOrO.js";import{C as e}from"./card_header-BeJQ3nT9.js";import{C as m}from"./contact_info-UC5UXDiY.js";const x=({suppliers:o})=>r.jsxs(a,{className:"supplier-card",children:[r.jsx(e,{info:o}),r.jsx(s,{sx:{marginBottom:1}}),r.jsx(m,{phone:o.phoneNumber,email:o.email})]});export{x as S};
